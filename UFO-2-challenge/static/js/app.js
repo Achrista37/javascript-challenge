02ef6a7afad5ea451c5e82c60258c970a98f02da
andriani <a.christanty@gmail.com>
2022-04-01 13:50:15 +0800

    ..

--- UFO-2-challenge/static/js/app.js
+++ UFO-2-challenge/static/js/app.js
@@ -26,42 +26,92 @@ function datauponLoading(x)
 filterButton.on("click", runfilteredData);
 /// execute function upon change of value 
 
+var populateTable = (sighting) =>
+{
+    var row = tbody.append("tr");
+    row.append("td").text(sighting.datetime);
+    row.append("td").text(sighting.city);
+    row.append("td").text(sighting.state);
+    row.append("td").text(sighting.country);
+    row.append("td").text(sighting.shape);
+}
 
 function runfilteredData() 
+
 {
+ 
+
+
+
+
   var filteredData = ufoData;
+   // Prevent the page from refreshing
+   d3.event.preventDefault();
     tbody.html("");
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
+   
     
     // Select the input element and get the raw HTML node
     var inputdateElement = d3.select("#datetime");
     var inputcityElement = d3.select("#city");
-    var inputstateElement = d3.select("#state");
+    var inputstateElement = d3.select("#State");
     var inputcountryElement = d3.select("#country");
     var inputshapeElement = d3.select("#shape");
 
     //check in console
-    console.log(inputdateElement)
+    console.log(inputdateElement);
 
   
     // Get the value property of the input element
-    var inputdateValue = inputdateElement.property("value");
-    var inputcityValue = inputcityElement.property("value");
-    var inputcountryValue = inputcountryElement.property("value");
-    var inputstateValue = inputstateElement.property("value");
-    var inputshapeValue = inputshapeElement.property("value");
+    var inputdateValue = inputdateElement.property("value").trim();
+    console.log(inputdateValue);
+    var inputcityValue = inputcityElement.property("value").toLowerCase().trim();
+    console.log(inputcityValue);
+    var inputcountryValue = inputcountryElement.property("value").toLowerCase().trim();
+    console.log(inputcountryValue)
+    var inputstateValue = inputstateElement.property("value").toLowerCase.trim();
+    console.log(inputstateValue)
+    var inputshapeValue = inputshapeElement.property("value").toLowerCase().trim();
+    console.log(inputshapeValue)
  
  
-  //set filter values (date, city, state, country, shape)
+  //set filter values (date, city, state, country, shape) and apply each filter individually to each category
+  //store each filtered result in different variable 
+  var filterBYDATE = ufoData.filter(data1 => ufoData.datetime === inputdateValue);
+  console.log(filterBYDATE);
+  var filterBYCITY = ufoData.filter(data2 => ufoData.city === inputcityValue);
+  console.log(filterBYCITY);
+  var filterBYSTATE = ufoData.filter(data3 => ufoData.State === inputstateValue);
+  console.log(filterBYSTATE);
+  var filterBYCOUNTRY = ufoData.filter(data4 => ufoData.country === inputcountryValue);
+  console.log(filterBYCOUNTRY);
+  var filterBYSHAPE = ufoData.filter(data5 => ufoData.country === inputcountryValue);
+  console.log(filterBYSHAPE);
+  var filterALL = ufoData.filter
+                        (data6 => ufoData.datetime === inputdateValue
+                        && ufoData.city === inputcityValue
+                        && ufoData.state === inputstateValue
+                        && ufoData.country === inputcountryValue
+                        && ufoData.shape === inputshapeValue      
+                        )
+  console.log(filterALL)
 
-  var filteredufoData = ufoData.filter(childufoData => ufoData.datetime === inputdateValue
+  var resultdata = {filterALL, filterBYDATE, filterBYCITY, filterBYSTATE, filterBYCOUNTRY, filterBYSHAPE}
+  if (resultdata.filterALL.length !== 0) {
+    populateTable(filterALL);
+  }
+  else if (response.filterALL.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0))){
+    populate(filterCity) || populate(filterDate);
+
+  }
+
+  /*                      
+  ufoData.filter(childufoData => ufoData.datetime === inputdateValue
                                                 && ufoData.city === inputcityValue
                                                 && ufoData.state === inputstateValue
                                                 && ufoData.country === inputcountryValue
                                                 && ufoData.shape === inputshapeValue      
     );
-  
+  */
 
 /////objects filter ****************THIS********************
 
@@ -69,13 +119,8 @@ function runfilteredData()
   console.log(filteredufoData);
   
   //populate table with filtered UFO data
-  filteredufoData.forEach((sighting) => {
-      var row = tbody.append("tr");
-      Object.entries(sighting).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-      });
-    });
+
+  
 
   }
 
